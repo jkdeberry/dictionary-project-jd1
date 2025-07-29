cc6cb97307edbd6f413b0672562024a500ff88f5
jkdeberry <debejmac@gmail.com>
2025-07-28 21:13:30 -0400

    Final style changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>DeBerry's Dictionary App</h2>
       </header>
       <main>
         <Dictionary />
