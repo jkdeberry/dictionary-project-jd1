5468fd9b22dbfbb8d4520d7a9d3f6ef1548fea6a
jkdeberry <debejmac@gmail.com>
2025-08-04 13:18:23 -0400

    Removed logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -8,7 +7,6 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h2>DeBerry's Dictionary App</h2>
       </header>
       <main>
